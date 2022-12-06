77a2f353140777f3f206310235281d10a44218dd
GuterKase <2916675@gmail.com>
2022-12-06 15:53:53 +0300

    Refactored Movement

--- data/Scripts/Movement/MoveInputSystem.cs
+++ data/Scripts/Movement/MoveInputSystem.cs
@@ -1,9 +1,10 @@
 using Leopotam.EcsLite;
 using Unigine;
 
-public class MovementInputSystem : IEcsInitSystem, IEcsRunSystem
+public class MoveInputSystem : IEcsInitSystem, IEcsRunSystem
 {
     private EcsPool<Movement> _movementPool;
+
     private vec2 _input = new();
 
     public void Init(IEcsSystems systems)
@@ -15,18 +16,25 @@ public class MovementInputSystem : IEcsInitSystem, IEcsRunSystem
 
     public void Run(IEcsSystems systems)
     {
+        GetInput();
+
         var world = systems.GetWorld();
 
-        EcsFilter filter = world.Filter<Movement>().End();
+        EcsFilter filter = world.Filter<GameObject>().Inc<Movement>().End();
 
         foreach (int entity in filter)
         {
             ref var movement = ref _movementPool.Get(entity);
 
-            _input.x = InputExtensions.GetAxis(Input.KEY.D, Input.KEY.A);
-            _input.y = InputExtensions.GetAxis(Input.KEY.W, Input.KEY.S);
-
             movement.Input = _input;
         }
+    }
+
+    private void GetInput()
+    {
+        _input.x = InputExtensions.GetAxis(Input.KEY.D, Input.KEY.A);
+        _input.y = InputExtensions.GetAxis(Input.KEY.W, Input.KEY.S);
+
+        _input.Normalize();
     }
 }
--- data/Scripts/Movement/Movement.cs
+++ data/Scripts/Movement/Movement.cs
@@ -8,6 +8,5 @@ public struct Movement
     private float _speed;
 
     public float Speed { get => _speed; set => _speed = value; }
-    public Node Node { get; set; }
     public vec2 Input { get; set; }
 }
