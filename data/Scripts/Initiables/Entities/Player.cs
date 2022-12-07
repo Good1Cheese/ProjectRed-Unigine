b625f288c700dcd4e5bd29b134cb1cdd78c1b6aa
GuterKase <2916675@gmail.com>
2022-12-07 08:29:08 +0300

    Shorted player spawn

--- data/Scripts/Initiables/Entities/Player.cs
+++ data/Scripts/Initiables/Entities/Player.cs
@@ -17,14 +17,13 @@ public class Player : Component, Initiable
 
     public void Initialize(EcsWorld world, EcsSystems systems)
     {
-        Node node = _nodeLink.Load(vec3.UP, quat.IDENTITY);
+        Node node = _nodeLink.Load(vec3.UP);
         var camera = node.GetChild(0) as PlayerDummy;
 
         int entity = world.NewEntity();
 
         _gameObject = new(node, camera);
         world.Add(entity, _gameObject);
-
         world.Add(entity, _movement);
         world.Add(entity, _rotation);
     }
